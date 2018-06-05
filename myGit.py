a094c51f8ee4f2b08c13dc3887847839fe47fc08
Luiz Cartolano <duducartolano@gmail.com>
2018-06-05 19:26:42 -0300

    adding read_index func

--- myGit.py
+++ myGit.py
@@ -51,13 +51,59 @@ class MyGit(object):
 		"""Compute hash of object data of given type and write to object store
     	if "write" is True. Return SHA-1 object hash as hex string.
     	"""
-    	header = '{},{}'.format(obj_type, len(data)).encode()
-    	full_data = header + b'\x00' + data
-    	sha1 = hashlib.sha1(full_data).hexdigest()
-    	if write:
-    		path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
-    		if not os.path.exists(path):
-    			os.makedirs(os.path.dirname(path), exist_ok = True)
-    			self.write_file(path, zlib.compress(full_data))
+		header = '{},{}'.format(obj_type, len(data)).encode()
+		full_data = header + b'\x00' + data
+		sha1 = hashlib.sha1(full_data).hexdigest()
+		if write:
+			path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
+			if not os.path.exists(path):
+				os.makedirs(os.path.dirname(path), exist_ok = True)
+				self.write_file(path, zlib.compress(full_data))
 
-    	return sha1		
+		return sha1
+
+	def read_index(self):
+		"""Read git index file and return list of IndexEntry objects."""
+
+		try:
+			data = self.read_file(os.path.join('.git', 'index'))
+		except FileNotFoundError:
+			return []
+
+		digest = hashlib.sha1(data[:-20]).digest()
+		assert digest == data[-20:], 'invalid index checksum'
+		signature, version, num_entries = struct.unpack('!4sLL', data[:12])
+		assert signature == b'DIRC', \
+			'invalid index signature {}'.format(signature)
+		assert version == 2, 'unknown index version {}'.format(version)
+		entry_data = data[12:-20]
+		entries = []
+		i = 0
+		while i + 62 < len(entry_data):
+			fields_end = i + 62
+			fields = struct.unpack('!LLLLLLLLLL20sH', entry_data[i:fields_end])
+			path_end = entry_data.index(b'\x00', fields_end)
+			path = entry_data[fields_end:path_end]
+			entry = IndexEntry(*(fields + (path.decode(),)))
+			entries.append(entry)
+			entry_len = ((62 + len(path) + 8) // 8) * 8
+			i += entry_len
+		
+		assert len(entries) == num_entries
+		return entries
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
