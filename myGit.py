8ee00e77f4521f250c9e41a0586b75fe336f65df
Luiz Cartolano <duducartolano@gmail.com>
2018-06-05 19:17:41 -0300

    adding hash function

--- myGit.py
+++ myGit.py
@@ -46,3 +46,18 @@ class MyGit(object):
 			os.mkdir(os.path.join(repo, '.git', name))
 		self.write_file(path=os.path.join(repo, '.git', 'HEAD'),data=b'ref: refs/heads/master')
 		print('initialized empty repository: {}'.format(repo))
+
+	def hash_object(self, data, obj_type, write=True):
+		"""Compute hash of object data of given type and write to object store
+    	if "write" is True. Return SHA-1 object hash as hex string.
+    	"""
+    	header = '{},{}'.format(obj_type, len(data)).encode()
+    	full_data = header + b'\x00' + data
+    	sha1 = hashlib.sha1(full_data).hexdigest()
+    	if write:
+    		path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
+    		if not os.path.exists(path):
+    			os.makedirs(os.path.dirname(path), exist_ok = True)
+    			self.write_file(path, zlib.compress(full_data))
+
+    	return sha1		
